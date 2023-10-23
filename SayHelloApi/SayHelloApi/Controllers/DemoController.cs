d111acd36a0c6fc9ec82118393449602954e4c31
Pardha <mailtosaradhi@gmail.com>
2023-10-23 10:59:48 +0530

    Adding SayHelloAPi application

---
+++ SayHelloApi/SayHelloApi/Controllers/DemoController.cs
@@ -0,0 +1,23 @@
+﻿    using Microsoft.AspNetCore.Mvc;
+
+namespace SayHelloApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class DemoController : Controller
+    {
+        private readonly ILogger<DemoController> _logger;
+
+        
+        public DemoController(ILogger<DemoController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult SayHello (string name)
+        {
+            string hello = "Hello " + name + "!!";
+            return Ok(hello);
+        }
+    }
+}
